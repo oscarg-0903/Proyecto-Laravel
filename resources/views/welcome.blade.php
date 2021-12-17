3b905904e31729b99c84cecad6977e47269cdded
Proyectos <oscarg.jimenezp@gmail.com>
2021-12-17 17:31:20 -0500

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
                         </g>
                     </svg>
                 </div>
-                <h1> Hello world</h1>
+                <h1> Hello world Clone GIT</h1>
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
